260668317f67d39ce41a3dbd4635c5fd026117c1
athaun <asher.haun@outlook.com>
2021-10-10 17:58:49 -0500

    server side setup

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Rasberry-pi-light-control
-
-[Demo video on youtube](https://www.youtube.com/watch?v=v8WLEMYDLF4)
 # python-tv-remote
--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 from flask import Flask, render_template, request
 from flask import jsonify
-import RPi.GPIO as GPIO
+import pyautogui
+import time
+
     
 app = Flask(__name__)
+# keyboard = Controller()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.OUT)
-GPIO.output(23, GPIO.HIGH)
+playingVideo = False
 
-lightOn = False
+def checkPost (request, data):
+    return request.get_data() ==  data
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    global lightOn
-    print(request.method)
+    global playingVideo
 
     if request.method == 'POST':
-        if request.get_data() == b'lightSwitch':
-            lightOn = (not lightOn)
-            if lightOn:
-                print("The light is now on")
-                GPIO.output(23, GPIO.HIGH)
-            else:
-                print("The light is now off")
-                GPIO.output(23, GPIO.LOW)
-        else:
-            return render_template("index.html", css="static/css/index.css", favicon="static/images/favicon.png", on="static/images/light-on.png", off="static/images/light-off.png", lightOn=lightOn)
-    return render_template("index.html", css="static/css/index.css", favicon="static/images/favicon.png", on="static/images/light-on.png", off="static/images/light-off.png", lightOn=lightOn)
+        if checkPost(request, b'play_pause'):
+            playingVideo = (not playingVideo)
+            if playingVideo:
+                print("The video is now playing")
 
-@app.route('/_stuff', methods=['GET'])
+            else:
+                print("The video is now paused")
+
+        if checkPost(request, b'vol_up'):
+            print("Volume up")
+            pyautogui.press("volumeup")
+
+        if checkPost(request, b'vol_dn'):
+            print("Volume down")
+            pyautogui.press("volumedown")
+
+    return render_template("index.html", css="static/css/index.css", favicon="static/images/favicon.png")
+
+@app.route('/_pause', methods=['GET'])
 def stuff():
-    return jsonify(lightOn=lightOn)  
+    return jsonify(playingVideo=playingVideo)  
 
 if __name__ == '__main__':
-    app.run(host="192.168.1.20", port=7070)
+    app.run(host="192.168.1.10", port=7070, debug=True)
 
