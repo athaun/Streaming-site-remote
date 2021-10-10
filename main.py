a2c06c29fbe9022ab8c7b4f29fa0b9e8bbfbdf01
athaun <asher.haun@outlook.com>
2021-10-10 18:26:35 -0500

    websites

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask import jsonify
 import pyautogui
 import time
+import webbrowser
 
     
 app = Flask(__name__)
@@ -9,8 +10,20 @@ app = Flask(__name__)
 
 playingVideo = False
 
+urls = [
+    [b"netflix",    "https://netflix.com"],
+    [b"hulu",       "https://hulu.com"],
+    [b"amazon",     "https://www.amazon.com/Amazon-Video/b/?ie=UTF8&node=2858778011"],
+    [b"youtube",    "https://youtube.com"],
+    [b"hbo",        "https://hbomax.com"],
+    [b"disney_plus","https://disneyplus.com"],
+    [b"spotify",    "https://open.spotify.com"],
+    [b"pandora",    "https://pandora.com"],
+    [b"google",     "https://google.com"]
+]
+
 def checkPost (request, data):
-    return request.get_data() ==  data
+    return request.get_data() == data
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
@@ -40,6 +53,11 @@ def index():
         if checkPost(request, b'right_arrow'):
             print("Right Arrow")
             pyautogui.press("right")
+    
+        for i in urls:
+            if checkPost(request, i[0]):
+                webbrowser.open(i[1], new=2)
+
 
     return render_template("index.html", css="static/css/index.css", favicon="static/images/favicon.png")
 
