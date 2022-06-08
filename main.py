646e29e7f31b31af2253c580bfdce09a5f60bbcc
Asher <asher.haun@outlook.com>
2022-06-08 18:33:38 -0500

    change to socketIO, darwin volume works

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit  
 from flask import jsonify
 import pyautogui
-import time
 import webbrowser
+import socket
+import json
+import platform
+import osascript
 
-    
+
 app = Flask(__name__)
-# keyboard = Controller()
+app.config['SECRET_KEY'] = 'secret!'                                            
+socketio = SocketIO(app) 
 
 playingVideo = False
 
 urls = [
-    [b"netflix",    "https://netflix.com"],
-    [b"hulu",       "https://hulu.com"],
-    [b"amazon",     "https://www.amazon.com/Amazon-Video/b/?ie=UTF8&node=2858778011"],
-    [b"youtube",    "https://youtube.com"],
-    [b"hbo",        "https://hbomax.com"],
-    [b"disney_plus","https://disneyplus.com"],
-    [b"spotify",    "https://open.spotify.com"],
-    [b"pandora",    "https://pandora.com"],
-    [b"google",     "https://google.com"]
+    ["netflix",    "https://netflix.com"],
+    ["hulu",       "https://hulu.com"],
+    ["amazon",     "https://www.amazon.com/Amazon-Video/b/?ie=UTF8&node=2858778011"],
+    ["youtube",    "https://youtube.com"],
+    ["hbo",        "https://hbomax.com"],
+    ["disney_plus","https://disneyplus.com"],
+    ["spotify",    "https://open.spotify.com"],
+    ["pandora",    "https://pandora.com"],
+    ["google",     "https://google.com"]
 ]
 
-import socket
+def darwinVolume (direction):
+    result = osascript.osascript('get volume settings')
+    info = result[1].split(',')
+    currentVolume = int(info[0].replace('output volume:', ''))
+
+    if direction == "up":
+       currentVolume += 5
+    elif direction == "down":
+        currentVolume -= 5
+
+    osascript.osascript("set volume output volume " + str(currentVolume))
+    
+
 def getip ():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -36,50 +53,64 @@ def getip ():
     return IP
 
 def checkPost (request, data):
-    return request.get_data() == data
+    return request == data
 
-@app.route("/", methods=['POST', 'GET'])
-def index ():
+@socketio.on('connect')                                                         
+def connect():               
+    print("Socket connected")   
+
+@socketio.on('message')
+def handleMessage (data):
+    message = data["data"]
+  
+
     global playingVideo
 
-    if request.method == 'POST':
-        if checkPost(request, b'play_pause'):
-            pyautogui.press(" ")
-            playingVideo = (not playingVideo)
-            if playingVideo:
-                print("The video is now playing")
-            else:
-                print("The video is now paused")
+    if checkPost(message, 'play_pause'):
+        pyautogui.press(" ")
+        playingVideo = (not playingVideo)
+        if playingVideo:
+            print("The video is now playing")
+        else:
+            print("The video is now paused")
+        emit('message', "DUMMY", broadcast=True)
 
-        if checkPost(request, b'vol_up'):
-            print("Volume up")
+
+    if checkPost(message, 'vol_up'):
+        print("Volume up")
+        if platform.system() == "Darwin":
+            darwinVolume("up")
+        else:
             pyautogui.press("volumeup")
 
-        if checkPost(request, b'vol_dn'):
-            print("Volume down")
+    if checkPost(message, 'vol_dn'):
+        print("Volume down")
+        if platform.system() == "Darwin":
+            darwinVolume("down")
+        else:
             pyautogui.press("volumedown")
 
-        if checkPost(request, b'left_arrow'):
-            print("Left Arrow")
-            pyautogui.press("left")
+    if checkPost(message, 'left_arrow'):
+        print("Left Arrow")
+        pyautogui.press("left")
 
-        if checkPost(request, b'right_arrow'):
-            print("Right Arrow")
-            pyautogui.press("right")
-    
-        for i in urls:
-            if checkPost(request, i[0]):
-                webbrowser.open(i[1], new=2)
+    if checkPost(message, 'right_arrow'):
+        print("Right Arrow")
+        pyautogui.press("right")
 
+    for i in urls:
+        if checkPost(message, i[0]):
+            webbrowser.open(i[1], new=2)
 
-    return render_template("index.html", css="static/css/index.css", favicon="static/images/favicon.png")
+@app.route("/", methods = ['POST', 'GET'])
+def index ():
+    return render_template("index.html", css="static/css/index.css")
 
 @app.route('/_pause', methods=['GET'])
 def stuff ():
     return jsonify(playingVideo=playingVideo)  
 
 if __name__ == '__main__':
-    print(f"\nOn your phone go to http://{getip()}\n")
-    time.sleep(2.50)
-    app.run(host=getip(), port=80, debug=True)
+    print(f"\nOn your phone go to http://{getip()}:8080\n")
+    socketio.run(app, host = "0.0.0.0", port = 8080, debug = True)
 
